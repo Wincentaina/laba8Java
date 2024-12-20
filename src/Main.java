61675b161cec4fc84df99130bc868bdd70c8e252
Wincentaina <103582986+Wincentaina@users.noreply.github.com>
2024-12-20 22:50:54 +0700

    Добавлен класс TestCaseInputComparator, функция поиска findTestCaseByExpected, обновлен метод main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 // Интерфейс для тестовых раннеров
 interface ITestRunner {
@@ -54,304 +53,79 @@ class TestCase {
     }
 
     // Метод для выполнения теста с использованием конкретного раннера
-    public ExecutionResult executeTest() {
-        ExecutionResult result = new ExecutionResult();
-        boolean isPassed = testRunner.executeTest(input, expected);
-        result.setActualOutput(input);
-        result.setIsPassed(isPassed);
-        return result;
+    public boolean executeTest() {
+        return testRunner.executeTest(input, expected);
     }
 }
 
-// Класс для представления набора тестов
-class TestSuite {
-    private static int totalTestSuitesCreated = 0;
-    private final List<TestCase> tests;
-
-    public TestSuite(List<TestCase> tests) {
-        this.tests = new ArrayList<>(tests);
-        totalTestSuitesCreated++;
-    }
-
-    public List<TestCase> getTests() {
-        return tests;
-    }
-
-    public int getTestCount() {
-        return tests.size();
-    }
-
-    public static int getTotalTestSuitesCreated() {
-        return totalTestSuitesCreated;
-    }
-}
-
-// Класс для представления задачи
-class Task {
+// Базовый класс для тестов с описанием
+class DescriptiveTestCase extends TestCase {
     private final String description;
-    private final TestSuite testSuite;
 
-    public Task(String description, TestSuite testSuite) {
+    public DescriptiveTestCase(String input, String expected, String description, ITestRunner testRunner) {
+        super(input, expected, testRunner);
         this.description = description;
-        this.testSuite = testSuite;
     }
 
     public String getDescription() {
         return this.description;
     }
-
-    public TestSuite getTestSuite() {
-        return this.testSuite;
-    }
 }
 
-// Класс для представления решения пользователя
-class UserSolution {
-    private final String solutionCode;
-
-    public UserSolution(String solutionCode) {
-        this.solutionCode = solutionCode;
-    }
-
-    public String getSolutionCode() {
-        return this.solutionCode;
-    }
-}
-
-// Класс для представления результата выполнения теста
-class ExecutionResult {
-    private String actualOutput;
-    private boolean isPassed;
-
-    public ExecutionResult() {
-        this.isPassed = false;
-    }
-
-    public void setActualOutput(String actualOutput) {
-        this.actualOutput = actualOutput;
-    }
-
-    public String getActualOutput() {
-        return actualOutput;
-    }
-
-    public void setIsPassed(boolean isPassed) {
-        this.isPassed = isPassed;
-    }
-
-    public boolean getIsPassed() {
-        return isPassed;
-    }
-}
-
-// Класс для представления отправки решения
-class Submission {
-    private final UserSolution solution;
-    private final List<ExecutionResult> results;
-    private int totalPassed;
-
-    public Submission(UserSolution solution, int testCount) {
-        this.solution = solution;
-        this.results = new ArrayList<>(testCount);
-        for (int i = 0; i < testCount; i++) {
-            results.add(new ExecutionResult());
-        }
-        this.totalPassed = 0;
-    }
-
-    public void setTotalPassed(int totalPassed) {
-        this.totalPassed = totalPassed;
-    }
-
-    public int getTotalPassed() {
-        return totalPassed;
-    }
-
-    public List<ExecutionResult> getResults() {
-        return results;
-    }
-
-    public UserSolution getSolution() {
-        return this.solution;
-    }
-}
-
-// Абстрактный класс для сущностей тестов
-abstract class TestEntity {
-    protected String id;
-
-    public TestEntity(String id) {
-        this.id = id;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public abstract void execute();
-}
-
-// Класс для расширенного теста
-class ExtendedTestCase extends TestCase {
-    private final String description;
-
-    public ExtendedTestCase(String input, String expected, String description) {
-        super(input, expected, new SimpleTestRunner()); // По умолчанию используем SimpleTestRunner
-        this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
+// Класс для сортировки тестов по полю input
+class TestCaseInputComparator implements Comparator<TestCase> {
     @Override
-    public String getInput() {
-        return "Description: " + description + ", Input: " + super.getInput();
-    }
-}
-
-// Класс для подробной задачи
-class DetailedTask extends Task {
-    private final String details;
-
-    public DetailedTask(String description, TestSuite testSuite, String details) {
-        super(description, testSuite);
-        this.details = details;
-    }
-
-    public String getDetails() {
-        return details;
-    }
-
-    @Override
-    public String getDescription() {
-        return super.getDescription() + " | Details: " + details;
-    }
-}
-
-// Класс для улучшенного набора тестов
-class AdvancedTestSuite extends TestSuite {
-    public AdvancedTestSuite(List<TestCase> tests) {
-        super(tests);
-    }
-
-    @Override
-    public int getTestCount() {
-        return super.getTestCount() + 1; // Дополнительный бонусный тест
-    }
-}
-
-// Интерфейс для клонирования
-interface ClonableEntity {
-    ClonableEntity shallowClone();
-    ClonableEntity deepClone();
-}
-
-// Класс для клонируемой задачи
-class CloneableTask extends Task implements ClonableEntity {
-    public CloneableTask(String description, TestSuite testSuite) {
-        super(description, testSuite);
-    }
-
-    @Override
-    public ClonableEntity shallowClone() {
-        return this; // Поверхностное клонирование
-    }
-
-    @Override
-    public ClonableEntity deepClone() {
-        return new CloneableTask(this.getDescription(), this.getTestSuite()); // Глубокое клонирование
-    }
-}
-
-// Класс для виртуальной задачи
-class VirtualTask extends Task {
-    public VirtualTask(String description, TestSuite testSuite) {
-        super(description, testSuite);
-    }
-
-    @Override
-    public String getDescription() {
-        return "Virtual: " + super.getDescription();
-    }
-}
-
-// Класс для проверки реализации интерфейса
-class InterfaceImplementation implements ClonableEntity {
-    private final String data;
-
-    public InterfaceImplementation(String data) {
-        this.data = data;
-    }
-
-    @Override
-    public ClonableEntity shallowClone() {
-        return new InterfaceImplementation(this.data);
-    }
-
-    @Override
-    public ClonableEntity deepClone() {
-        return new InterfaceImplementation(new String(this.data));
-    }
-
-    public String getData() {
-        return data;
+    public int compare(TestCase t1, TestCase t2) {
+        return t1.getInput().compareTo(t2.getInput());
     }
 }
 
 public class Main {
 
-    public static ExecutionResult runTestCase(UserSolution solution, TestCase test) {
-        return test.executeTest(); // Используем метод executeTest, который уже использует нужный раннер
-    }
-
-    public static Submission checkSolution(UserSolution solution, Task task) {
-        Submission submission = new Submission(solution, task.getTestSuite().getTestCount());
-
-        int totalPassed = 0;
-        for (int i = 0; i < task.getTestSuite().getTestCount(); i++) {
-            TestCase test = task.getTestSuite().getTests().get(i);
-            ExecutionResult result = runTestCase(solution, test);
-            submission.getResults().set(i, result);
-            if (result.getIsPassed()) {
-                totalPassed++;
+    // Алгоритм поиска теста по значению expected
+    public static TestCase findTestCaseByExpected(List<TestCase> testCases, String expected) {
+        for (TestCase testCase : testCases) {
+            if (testCase.getExpected().equals(expected)) {
+                return testCase;
             }
         }
-
-        submission.setTotalPassed(totalPassed);
-        return submission;
-    }
-
-    public static void executeTestEntity(TestEntity entity) {
-        entity.execute(); // Вызов виртуальной функции
+        return null; // Если тест не найден
     }
 
     public static void main(String[] args) {
-        // Создаем тесты с разными раннерами
-        List<TestCase> tests = new ArrayList<>();
-        tests.add(new TestCase("input1", "expected1", new SimpleTestRunner()));  // Используем SimpleTestRunner
-        tests.add(new TestCase("input2", "input2", new AdvancedTestRunner(3)));  // Используем AdvancedTestRunner с уровнем сложности 3
+        // Создаем контейнеры для тестов
+        List<TestCase> testCases = new ArrayList<>();
+        List<DescriptiveTestCase> descriptiveTestCases = new ArrayList<>();
 
-        TestSuite testSuite = new TestSuite(tests);
+        // Добавляем тесты
+        testCases.add(new TestCase("input1", "expected1", new SimpleTestRunner()));
+        testCases.add(new TestCase("input2", "input2", new AdvancedTestRunner(3)));
 
-        // Создаем задачу с тестами
-        Task task = new Task("Task with Multiple Runners", testSuite);
+        descriptiveTestCases.add(new DescriptiveTestCase("input3", "expected3", "Test A", new SimpleTestRunner()));
+        descriptiveTestCases.add(new DescriptiveTestCase("input4", "expected4", "Test C", new AdvancedTestRunner(5)));
+        descriptiveTestCases.add(new DescriptiveTestCase("input5", "expected5", "Test B", new SimpleTestRunner()));
 
-        // Проверяем решение
-        UserSolution userSolution = new UserSolution("user solution code");
+        // Алгоритм сортировки тестов по полю input
+        testCases.sort(new TestCaseInputComparator());
+        System.out.println("Sorted test cases by input:");
+        for (TestCase testCase : testCases) {
+            System.out.println(testCase.getInput());
+        }
 
-        Submission submission = checkSolution(userSolution, task);
-        System.out.println("Total Passed: " + submission.getTotalPassed());
+        // Поиск теста по значению expected
+        String searchExpected = "expected3";
+        TestCase foundTest = findTestCaseByExpected(testCases, searchExpected);
 
-        // Демонстрация дополнительных классов
-        ExtendedTestCase extendedTestCase = new ExtendedTestCase("input1", "expected1", "Test with description");
-        System.out.println(extendedTestCase.getInput());
+        if (foundTest != null) {
+            System.out.println("Found test with expected: " + foundTest.getExpected());
+        } else {
+            System.out.println("Test with expected '" + searchExpected + "' not found.");
+        }
 
-        AdvancedTestSuite advancedSuite = new AdvancedTestSuite(tests);
-        System.out.println("Advanced suite test count: " + advancedSuite.getTestCount());
-
-        // Виртуальные функции
-        VirtualTask virtualTask = new VirtualTask("Virtual Task", testSuite);
-        System.out.println(virtualTask.getDescription());
+        // Выполняем тесты и выводим результаты
+        for (TestCase testCase : testCases) {
+            boolean result = testCase.executeTest();
+            System.out.println("Test with input: " + testCase.getInput() + " passed: " + result);
+        }
     }
 }
